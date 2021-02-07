373e5015fc856bfc277be2300bb3004f0d7c8625
bartek9105 <bartek9105@gmail.com>
2021-02-07 14:41:45 +0100

    Prepare for deployment

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const dbConnection = require('./db')
 const errorHandler = require('./errorHandler')
+const compression = require('compression')
+const helmet = require('helmet')
 require('dotenv').config()
 
 dbConnection()
@@ -12,9 +14,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(helmet())
+app.use(compression())
+
 app.use('/api/v1/stats', statsRoutes)
 
 app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000
 
